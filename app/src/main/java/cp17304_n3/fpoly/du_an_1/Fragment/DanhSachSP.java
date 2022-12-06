ba578de452b0ff940f6e1829a072e42e79cfa006
quan <quantdmph21259@fpt.edu.vn>
2022-12-06 08:17:06 +0700

    Revert "thêm đăng ký"
    This reverts commit 47c04e673f60b57efd3ac472ba5761a5f5811097.

--- app/src/main/java/cp17304_n3/fpoly/du_an_1/Fragment/DanhSachSP.java
+++ app/src/main/java/cp17304_n3/fpoly/du_an_1/Fragment/DanhSachSP.java
@@ -13,12 +13,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 import android.widget.ListView;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -70,7 +67,6 @@ public class DanhSachSP extends Fragment {
     private List<Photo> list_photo;
     private Timer timer;
 
-    private GridView gridView;
 
 
     @Override
@@ -84,7 +80,6 @@ public class DanhSachSP extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         id_lv_danhsachsp = view.findViewById(R.id.id_lv_danhsachsp);
-//        gridView = view.findViewById(R.id.gridview);
         id_viewpager = view.findViewById(R.id.id_viewpager);
        circle_indiacator = view.findViewById(R.id.circle_indiacator);
 
@@ -101,16 +96,17 @@ public class DanhSachSP extends Fragment {
         photo_adapter.registerDataSetObserver(circle_indiacator.getDataSetObserver());
 
         autoSLideImages();
-//        SachDao sachDao=new SachDao();
-//        List<Sach> listCat = sachDao.getAll(); // lấy danh sách cho vào biến
-//
-//        // duyệt mảng in ra danh sách
-//        for(int i = 0; i<listCat.size(); i++){
-//            Sach objCat = listCat.get(i);
-//
-//            Log.d("zzzzz", "onCreate: phần tử thứ " + i + ":  id = " + objCat.getIdSach() + ", name = " + objCat.getTenSach());
-//
-//        }
+        SachDao sachDao=new SachDao();
+        List<Sach> listCat = sachDao.getAll(); // lấy danh sách cho vào biến
+
+        // duyệt mảng in ra danh sách
+        for(int i = 0; i<listCat.size(); i++){
+            Sach objCat = listCat.get(i);
+
+            Log.d("zzzzz", "onCreate: phần tử thứ " + i + ":  id = " + objCat.getIdSach() + ", name = " + objCat.getTenSach());
+
+        }
+
 
 
 
